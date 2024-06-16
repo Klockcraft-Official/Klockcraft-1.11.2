3c3134fb3d533b1710767fe5824ee1f0c7f8e1b6
Brady Trudel <bradytrudel@outlook.com>
2024-06-16 09:55:44 -0400

    mouse handling

--- minecraft.py
+++ minecraft.py
@@ -1,5 +1,7 @@
 import turtle
 import threading
+import pyautogui
+import keyboard
 
 # Constants
 SCREEN_WIDTH = 800
@@ -12,10 +14,13 @@ ON_INVENTORY_SLOT = 1
 HEALTH = 100
 HUNGER = 100
 XP = 0
+MOUSE_X = 0
+MOUSE_Y = 0
 
 # Initialize screen
 screen = turtle.Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.cv._rootwindow.config(cursor="none")
 screen.title("Minecraft")
 
 screen.tracer(0)  # Disable screen updates
@@ -28,6 +33,12 @@ crosshair_turtle.hideturtle()
 
 inventory_slot_turtle.speed(0)
 inventory_slot_turtle.hideturtle()
+
+def track_mouse():
+    global MOUSE_X, MOUSE_Y
+    while True:
+        MOUSE_X, MOUSE_Y = pyautogui.position()
+        # print('Mouse position:', MOUSE_X, MOUSE_Y)
 
 # Function to draw the crosshair
 def draw_crosshair():
@@ -66,6 +77,10 @@ def draw_inventory_slots():
         inventory_slot_turtle.setheading(270)
         inventory_slot_turtle.forward(50)
         inventory_slot_turtle.penup()
+
+mouse_track_thread = threading.Thread(target=track_mouse)
+mouse_track_thread.daemon = True
+mouse_track_thread.start()
 
 # Crosshair turtle
 turtle.speed(0)  # Set fastest animation speed
